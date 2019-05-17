6a2ca42e34f6e610eb998abec38c9329de8fe9ae
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-05-17 11:55:47 -0700

    Create switch_case.cpp

---
+++ switch_case.cpp
@@ -0,0 +1,25 @@
+switch (condition)
+{
+    case 1: // code to be executed if n = 1;
+        break;
+    case 2: // code to be executed if n = 2;
+        break;
+    default: // code to be executed if n doesn't match any cases
+}
+
+int main() 
+{ 
+   int x = 2; 
+   switch (x) 
+   { 
+       case 1: printf("Choice is 1"); 
+               break; 
+       case 2: printf("Choice is 2"); 
+                break; 
+       case 3: printf("Choice is 3"); 
+               break; 
+       default: printf("Choice other than 1, 2 and 3"); 
+                break;   
+   } 
+   return 0; 
+} 
